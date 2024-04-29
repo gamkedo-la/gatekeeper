2ec0fe4bcd6321c92232862fc9d0978085412a3b
fernandarobles1234 <fernandarobles1234@gmail.com>
2024-04-29 11:21:12 -0400

    Button Script

---
+++ Assets/Scripts/ResumeButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonScript : MonoBehaviour { 
+
+    public void OnClick()
+    {
+        print("button clicked");
+    }
+}
