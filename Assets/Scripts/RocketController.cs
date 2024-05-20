72556866c50d50722e350978c3afec1af7b53cb1
Alex Joiner <alexjoinerwebdev@gmail.com>
2024-05-20 16:36:06 -0500

    Boss rocket attack first pass

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -6,15 +6,26 @@ public class RocketController : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private Transform rocketRangeStart;
+    [SerializeField] private Transform rocketRangeEnd;
+    private bool hitTarget = false;
 
     void Start()
     {
-        
     }
 
-
-    void Update()
+    void FixedUpdate()
     {
-        rigidbody2D.transform.position = Vector3.Lerp(rigidbody2D.transform.position, target.position, .1f);
+        rigidbody2D.transform.position = Vector3.Lerp(rigidbody2D.transform.position, target.position, .10f);
+
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (Vector3.Distance(transform.position, target.position) <= 1 && !hitTarget)
+              {
+                  target.position = new Vector3(Random.Range(rocketRangeStart.position.x, rocketRangeEnd.position.x), Random.Range(rocketRangeStart.position.y, rocketRangeEnd.position.y), 0);
+
+                  hitTarget = true;
+              }
     }
 }
