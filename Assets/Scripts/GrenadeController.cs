1ed4296024b718db55b23659fc1cf372cafca379
Alex Joiner <alexjoinerwebdev@gmail.com>
2024-05-21 15:26:21 -0500

    Boss can throw grenades at random distance

--- Assets/Scripts/GrenadeController.cs
+++ Assets/Scripts/GrenadeController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class GrenadeController : MonoBehaviour
 {
+
+    [SerializeField] private Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    void Awake()
+    {
+        rigidbody2D.velocity = new Vector2(Random.RandomRange(-8,-2), 10);
         
     }
 
